bc737cfb650466bcec6b7855d82b9c2681c342f1
Daniel Sund <daniel.sund@calabrio.com>
2022-02-10 16:17:35 +0100

    Initital commit

---
+++ mini.api/Program.cs
@@ -0,0 +1,21 @@
+using mini.api.extensions;
+using mini.api.infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.SetupDatabase();
+var app = builder.Build();
+
+app.MigrateAndSeedDatabase();
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/items/", (HttpContext context) =>
+{
+    var dbContext = context.RequestServices.GetRequiredService<MiniApiDbContext>();
+    return dbContext.Items.ToList();
+});
+
+app.Run();
+
+public partial class Program { }
---
+++ mini.api/domain/Item.cs
@@ -0,0 +1,6 @@
+﻿namespace mini.api.domain;
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+}
